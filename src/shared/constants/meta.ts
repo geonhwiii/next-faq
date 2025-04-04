b87a3be675f2396cc55003623c13cfffb58b8d71
Dan <44767362+geonhwiii@users.noreply.github.com>
2025-04-04 22:41:55 +0900

    feat: initial setting

---
+++ src/shared/constants/meta.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from 'next';
+
+export const meta: Metadata = {
+	title: '서비스 도입 FAQ | 기아 비즈(Kia Biz) - 친환경 모빌리티 서비스',
+	description:
+		'기아 비즈는 기업을 위한 친환경 모빌리티 서비스로 차량부터 전용 App/Web까지 업무차량 토탈 솔루션을 제공합니다.',
+};
