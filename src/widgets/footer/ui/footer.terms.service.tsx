ea9fc5ae5b435459988a0bea98c255fefc6e334d
Dan <44767362+geonhwiii@users.noreply.github.com>
2025-04-09 02:29:51 +0900

    feat: terms select 적용

--- src/widgets/footer/ui/footer.terms.service.tsx
+++ src/widgets/footer/ui/footer.terms.service.tsx
@@ -1,12 +1,24 @@
+'use client';
+
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { termsQueryOptions } from '../api';
 import { Dialog } from '@/shared/ui/dialog';
 import { useState } from 'react';
 import { Select } from '@/shared/ui/select';
+import { formatTimestampToDate } from '@/shared/utils/date';
 
 export function FooterTermsService() {
 	const [isOpen, setIsOpen] = useState(false);
 	const { data } = useSuspenseQuery(termsQueryOptions({ termsClassId: 'JOIN_SERVICE_USE' }));
+	const [selectedVersion, setSelectedVersion] = useState<string>(data.terms[0].termsVersion.toString());
+
+	const options = data.terms.map((term) => ({
+		value: term.termsVersion.toString(),
+		label: `${formatTimestampToDate(term.startDate)} ~ ${term.endDate ? formatTimestampToDate(term.endDate) : '현재'}`,
+	}));
+
+	const __html = data?.terms.find((term) => term.termsVersion.toString() === selectedVersion)?.contents || '';
+
 	return (
 		<>
 			<button onClick={() => setIsOpen(true)}>
@@ -19,22 +31,11 @@ export function FooterTermsService() {
 					title="이용 약관"
 					content={
 						<>
-							<Select
-								className="mb-3"
-								value={'1726166400000'}
-								onChange={() => {}}
-								options={[
-									{ value: '1726166400000', label: '2024.09.12 ~ 현재' },
-									{ value: '1714348800000', label: '2024.04.29 ~ 2024.09.11' },
-									{ value: '1700748000000', label: '2023.11.24 ~ 2024.04.28' },
-									{ value: '1680451200000', label: '2023.04.03 ~ 2023.11.23' },
-									{ value: '1640444400000', label: '2021.12.26 ~ 2023.04.02' },
-								]}
-							/>
+							<Select className="mb-3" value={selectedVersion} onChange={setSelectedVersion} options={options || []} />
 							<div className="**:!text-[12px] **:lg:!text-[14px] **:leading-[var(--line-height-sm)] **:break-all">
 								<p
 									dangerouslySetInnerHTML={{
-										__html: data?.terms[0]?.contents || '',
+										__html,
 									}}
 								/>
 							</div>
